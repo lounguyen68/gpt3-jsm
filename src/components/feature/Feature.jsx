50dfb9cc566ecc7e00ab82a03c084337c4060114
lounguyen68 <lou9.works@gmail.com>
2023-01-30 20:57:36 +0700

    Build CTA, Features, Blog, Footer, ... done

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
 import './feature.css';
 
-const Feature = () => {
+const Feature = (props) => {
   return (
-    <div>Feature</div>
+    <div className="gpt3__features-container__feature">
+        <div className="gpt3__features-container__feature-title">
+          <div></div>
+          <h1>{props.title}</h1>
+        </div>
+        <div className="gpt3__features-container__feature-text">
+            <p>{props.text}</p>
+        </div>
+    </div>
   )
 }
 
